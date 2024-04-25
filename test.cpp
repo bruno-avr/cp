baacbf855ace756f8dc5f477a681240cf6f23b49
Bruno Rezende <brunobubat@yahoo.com.br>
2024-04-24 22:37:07 -0300

    more problems

--- test.cpp
+++ test.cpp
@@ -1,55 +1,62 @@
 #include "bits/stdc++.h"
-
-#define ll long long
-
 using namespace std;
 
-string CheckDOM(string str) {
-  vector<string> tokens;
-  
-  string newToken;
-  for (char &c : str) {
-    if (c == '<') newToken = "";
-    else if (c == '>') tokens.push_back(newToken);
-    else newToken += c;
-  }
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
 
-  string toBeChanged = "";
-  stack<string> st;
-  for (string &token : tokens) {
-    if (token[0] == '/') {
-      if (!st.size()) return "false";
-      if (st.top() == token.substr(1)) st.pop();
-      else if (toBeChanged != "") return "false";
-      else {
-        toBeChanged = st.top();
-        st.pop();
-      }
-    } else {
-      st.push(token);
-    }
-  }
+#define ll long long
+#define ld long double
+#define pii pair<ll,ll>
 
-  if (st.size()) return "false";
-  if (toBeChanged == "") return "true";
-   
-  return toBeChanged;
+#define fi first
+#define se second
+#define pb push_back
 
-}
+#define all(x) x.begin(),x.end()
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define mod 998244353LL
+
+#define f(i,s,e) for(ll i=s;i<e;i++)
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
 
 void solve() {
-    string str;
-    getline(cin, str);
-    // cout << str << endl;
-    cout << CheckDOM(str) << endl;
+    int n, k;
+    cin >> n >> k;
+    unordered_map<int, unordered_map<int, int>> um(2*n);
+    for (int i = 0; i < n; i++) um[i] = unordered_map<int, int>(2*(n-i));
+    um[0][0] = k-1;
+    int startCol = 0;
+    for (int row = 0; row < n; row++) {
+      for (int col = startCol; col < (n-row); col++) {
+            if (col > 0) um[row][col] += um[row][col-1] / 2;
+            if (row > 0) um[row][col] += (um[row-1][col] + 1) / 2;
+            if (col > 0) if (um[row][col] == 0 && um[row][col-1] == 0) break;
+            if (row > 0) if (um[row][col] == 1 && um[row-1][col] == 1) startCol++;
+        }
+    }
+    
+    int col = 0, row = 0;
+    while (row != n-1-col) {
+        if (um[row][col] % 2 == 0) { // down
+            row++;
+        } else { // right
+            col++;
+        }
+    }
+    cout << col << endl;
+
 }
 
 int main() {
-    ll t;
-    cin >> t;
-    cin.ignore();
+    optimize;
+    int T;
+    cin >> T;
 
-    while (t--) solve();
+    while (T--) solve();
 
     return 0;
 }
--- training/a.cpp
+++ training/a.cpp
@@ -1,76 +1,31 @@
 #include "bits/stdc++.h"
+ 
 using namespace std;
-
-#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define endl "\n"
-
-#define ll long long
-#define all(x) x.begin(),x.end()
-
-unordered_map<char, int> tab1;
-vector<char> tab2;
-
-void init () {
-    int cont = 0;
-    for (int i = '0'; i <= '9'; i++) {
-        tab2.push_back(i);
-        tab1[i] = cont++;
-    }
-    for (int i = 'a'; i <= 'z'; i++) {
-        tab2.push_back(i);
-        tab1[i] = cont++;
-    }
-    for (int i = 'A'; i <= 'Z'; i++) {
-        tab2.push_back(i);
-        tab1[i] = cont++;
-    }
-}
-
-void solve(vector<string> &v, int c) {
-    unordered_map<char, pair<int,int>> charPos;
-    int maxChar = 0;
-    for (int i = 0; i < v.size(); i++) {
-        for (int j = 0; j < v[i].size(); j++) {
-            if (v[i][j] != '.') {
-                maxChar = max(maxChar, tab1[v[i][j]]);
-                charPos[v[i][j]] = {i,j};
-            }
-        }
-    }
-    for (int i = 0; i < maxChar; i++) {
-        auto &[x1, y1] = charPos[tab2[i]];
-        auto &[x2, y2] = charPos[tab2[i+1]];
-        for (int j = min(x1, x2)+1; j < max(x1, x2); j++) {
-            if (v[j][y1] == '.') v[j][y1] = '|';
-            if (v[j][y1] == '-') v[j][y1] = '+';
-        }
-        for (int j = min(y1, y2)+1; j < max(y1, y2); j++) {
-            if (v[x1][j] == '.') v[x1][j] = '-';
-            if (v[x1][j] == '|') v[x1][j] = '+';
-        }
-    }
-    if (c) cout << endl;
-    for (auto line : v) {
-        cout << line << endl;
-    }
-}
-
+ 
 int main() {
-    optimize;
-    init();
-    
-    string s;
-    vector<string> v;
-    int c = 0;
-    while(getline(cin, s)) {
-        if (s == "") {
-            solve(v, c++);
-            v.clear();
-            continue;
+    int n, t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
         }
-        v.push_back(s);
+        reverse(v.begin(), v.end());
+ 
+        int maxR = 1;
+        unordered_set<int> s;
+        for (int i = 0; i < n; i++) {
+            int aux = 0;
+            if (s.count(v[i])) aux++;
+            s.insert(v[i]);
+            s.erase(n-i);
+            maxR = max(maxR, (int)s.size() + aux);
+        }
+ 
+        cout << maxR << endl;
     }
-    solve(v, c++);
-
+    
+    
     return 0;
 }
